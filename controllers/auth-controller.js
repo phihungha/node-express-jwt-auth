797588d3674f6c56c4e25930bd5bf7e0efc3fd75
Ha Phi Hung <haphihung55@gmail.com>
2023-02-14 20:53:49 +0700

    Add error handling and password hashing

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,4 +1,22 @@
-const User = require('../models/user');
+const User = require("../models/user");
+
+function handleError(err) {
+  console.log(err.message, err.code);
+
+  let errors = { email: "", password: "" };
+
+  if (err.code === 11000) {
+    errors.email = "Email is already registered";
+  }
+
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+}
 
 module.exports.signup_get = (req, res) => {
   res.render("signup");
@@ -8,11 +26,11 @@ module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.create({email, password});
+    const user = await User.create({ email, password });
     res.status(201).json(user);
-  } catch(err) {
-    console.log(err);
-    res.status(400).send('Error: User not created');
+  } catch (err) {
+    const errors = handleError(err);
+    res.status(400).json({ errors });
   }
 };
 
@@ -20,7 +38,7 @@ module.exports.login_get = (req, res) => {
   res.render("login");
 };
 
-module.exports.login_post =  async (req, res) => {
+module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
   console.log(email, password);
   res.send("new login");
--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
   email: {
@@ -6,13 +8,20 @@ const userSchema = mongoose.Schema({
     required: [true, "Please enter an email"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
   },
   password: {
     type: "string",
     required: [true, "Please enter a password"],
-    minLength: [6, "Minimum password length is 6"],
+    minlength: [6, "Minimum password length is 6"],
   },
 });
+
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+})
 
 const User = mongoose.model("user", userSchema);
 module.exports = User;
