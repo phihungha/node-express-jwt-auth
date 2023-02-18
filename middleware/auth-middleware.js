66f8f938f9c65a017b29e4e87339f4f4511d1b6d
Ha Phi Hung <haphihung55@gmail.com>
2023-02-18 17:21:50 +0700

    Add middleware to get current user info

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -17,4 +17,25 @@ function requireAuth(req, res, next) {
   }
 }
 
-module.exports = { requireAuth };
+function checkCurrentUser(req, res, next) {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.locals.user = null;
+        next();
+      } else {
+        console.log(decodedToken);
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+}
+
+module.exports = { requireAuth, checkCurrentUser };
