38762987412908ec412670663ff444bb4dda6498
Ha Phi Hung <haphihung55@gmail.com>
2023-02-18 16:55:27 +0700

    Implement login

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -9,10 +9,18 @@ function createJWT(id) {
   });
 }
 
-function handleError(err) {
+function handleErrors(err) {
   console.log(err.message, err.code);
 
   let errors = { email: "", password: "" };
+
+  if (err.message === "incorrect email") {
+    errors.email = "That email is not registered";
+  }
+
+  if (err.message === "incorrect password") {
+    errors.password = "Password is incorrect";
+  }
 
   if (err.code === 11000) {
     errors.email = "Email is already registered";
@@ -27,6 +35,24 @@ function handleError(err) {
   return errors;
 }
 
+module.exports.login_get = (req, res) => {
+  res.render("login");
+};
+
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const jwt = createJWT(user._id);
+    res.cookie("jwt", jwt, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+};
+
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
@@ -40,17 +66,7 @@ module.exports.signup_post = async (req, res) => {
     res.cookie("jwt", jwt, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(201).json({ user: user._id });
   } catch (err) {
-    const errors = handleError(err);
+    const errors = handleErrors(err);
     res.status(400).json({ errors });
   }
-};
-
-module.exports.login_get = (req, res) => {
-  res.render("login");
-};
-
-module.exports.login_post = async (req, res) => {
-  const { email, password } = req.body;
-  console.log(email, password);
-  res.send("new login");
 };
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
   email: {
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema({
     required: [true, "Please enter an email"],
     unique: true,
     lowercase: true,
-    validate: [isEmail, 'Please enter a valid email']
+    validate: [isEmail, "Please enter a valid email"],
   },
   password: {
     type: "string",
@@ -17,11 +17,24 @@ const userSchema = mongoose.Schema({
   },
 });
 
-userSchema.pre('save', async function(next) {
+userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt();
   this.password = await bcrypt.hash(this.password, salt);
   next();
-})
+});
+
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const isAuth = await bcrypt.compare(password, user.password);
+    if (isAuth) {
+      return user;
+    }
+    throw Error("incorrect password");
+  }
+
+  throw Error("incorrect email");
+};
 
 const User = mongoose.model("user", userSchema);
 module.exports = User;
